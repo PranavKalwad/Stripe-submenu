57be0818efd57a1f347e29f550d746ce8ae6b80b
unknown <pranavkalwad@gmail.com>
2022-07-09 19:41:44 +0530

    finished stripe menu

--- src/context.js
+++ src/context.js
@@ -1,44 +1,48 @@
-import React, { useState, useContext } from "react"
-import sublinks from "./data"
+import React, { useState, useContext } from 'react';
+import sublinks from './data';
+const AppContext = React.createContext();
 
-const AppContext = React.createContext()
-
-export const AppProvider = ({ children }) => {
-  const [isSidebarOpen, setIsSidebarOpen] = useState(true)
-  const [isSubmenuOpen, setIsSubmenuOpen] = useState(true)
-
+const AppProvider = ({ children }) => {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+  const [isSubmenuOpen, setIsSubmenuOpen] = useState(false);
+  const [page, setPage] = useState({ page: '', links: [] });
+  const [location, setLocation] = useState({});
   const openSidebar = () => {
-    setIsSidebarOpen(true)
-  }
-
+    setIsSidebarOpen(true);
+  };
   const closeSidebar = () => {
-    setIsSidebarOpen(false)
-  }
-
-  const openSubmenu = () => {
-    isSubmenuOpen(true)
-  }
-
+    setIsSidebarOpen(false);
+  };
+  const openSubmenu = (text, coordinates) => {
+    const page = sublinks.find((link) => link.page === text);
+    setPage(page);
+    setLocation(coordinates);
+    setIsSubmenuOpen(true);
+  };
   const closeSubmenu = () => {
-    setIsSubmenuOpen(false)
-  }
+    setIsSubmenuOpen(false);
+  };
 
   return (
-    <AppProvider
+    <AppContext.Provider
       value={{
-        isSubmenuOpen,
         isSidebarOpen,
-        openSubmenu,
         openSidebar,
-        closeSubmenu,
         closeSidebar,
+        isSubmenuOpen,
+        openSubmenu,
+        closeSubmenu,
+        page,
+        location,
       }}
     >
       {children}
-    </AppProvider>
-  )
-}
-
+    </AppContext.Provider>
+  );
+};
+// make sure use
 export const useGlobalContext = () => {
-  return useContext(AppContext)
-}
+  return useContext(AppContext);
+};
+
+export { AppContext, AppProvider };
